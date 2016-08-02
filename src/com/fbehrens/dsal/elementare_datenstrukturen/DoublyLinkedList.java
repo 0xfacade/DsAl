55551faa68714763bc8fcf570e7dea2d4e1f36a1
Florian Behrens <florian.behrens@rocketmail.com>
2016-08-02 21:57:30 +0200

    Add DoublyLinkedList and according test suite

---
+++ src/com/fbehrens/dsal/elementare_datenstrukturen/DoublyLinkedList.java
@@ -0,0 +1,139 @@
+package com.fbehrens.dsal.elementare_datenstrukturen;
+
+import java.util.Iterator;
+
+/**
+ * A doubly linked list uses nodes to store its values. Every node
+ * holds a value and a link to the previous and next nodes.
+ * This particular implementation always has a head node with a value of null,
+ * even when the list is actually empty. A lot of special cases
+ * do not have to be taken care of when accessing the head, therefore.
+ */
+public class DoublyLinkedList<VALUE> implements AbstractList<VALUE>{
+	
+	private static class Node<VALUE> {
+		public final VALUE value;
+		private Node<VALUE> previous, next;
+		
+		public Node(VALUE value){
+			this.value = value;
+		}
+		
+		public void delete(){
+			this.previous.next = this.next;
+			this.next.previous = this.previous;
+		}
+		
+		public void append(Node<VALUE> appendMe){
+			appendMe.next = this.next;
+			appendMe.previous = this;
+			this.next.previous = appendMe;
+			this.next = appendMe;
+		}
+	}
+	
+	private final Node<VALUE> head;
+	
+	public DoublyLinkedList(){
+		this.head = new Node<VALUE>(null);
+		this.head.next = this.head;
+		this.head.previous = this.head;
+	}
+	
+	@Override
+	public void append(VALUE value){
+		Node<VALUE> appendMe = new Node<>(value);
+		this.head.previous.append(appendMe);
+	}
+	
+	/**
+	 * Inserts the value at the beginning of the list.
+	 * This method is special to {@link DoublyLinkedList}:
+	 * usually, lists do not have this feature because
+	 * it cannot be efficiently implemented.
+	 */
+	public void prepend(VALUE value){
+		Node<VALUE> prependMe = new Node<>(value);
+		this.head.append(prependMe);
+	}
+	
+	@Override
+	public void delete(VALUE valueToDelete){
+		Node<VALUE> nodeToDelete = findFirstNodeWithValue(valueToDelete);
+		if(nodeToDelete != null){
+			nodeToDelete.delete();
+		}
+	}
+	
+	@Override
+	public boolean contains(VALUE toFind) {
+		return findFirstNodeWithValue(toFind) != null;
+	}
+	
+	private Node<VALUE> findFirstNodeWithValue(VALUE toFind){
+		Node<VALUE> currentlyAt = this.head.next;
+		while(currentlyAt != this.head){
+			if(toFind.equals(currentlyAt.value)){
+				return currentlyAt;
+			}
+			currentlyAt = currentlyAt.next;
+		}
+		return null;
+	}
+
+	@Override
+	public Iterator<VALUE> iterator() {
+		// an iterator is required so that this object can be used in
+		// for-each loops
+		return new Iterator<VALUE>(){
+			private Node<VALUE> currentlyAt = DoublyLinkedList.this.head.next;
+			@Override
+			public boolean hasNext(){
+				return currentlyAt != DoublyLinkedList.this.head;
+			}			
+			@Override
+			public VALUE next() {
+				VALUE value = currentlyAt.value;
+				currentlyAt = currentlyAt.next;
+				return value;
+			}
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		try {
+			DoublyLinkedList<VALUE> other = (DoublyLinkedList<VALUE>) obj;
+			Iterator<VALUE> selfIter = this.iterator(), otherIter = other.iterator();
+			while(selfIter.hasNext()){
+				if(!otherIter.hasNext()){
+					return false;
+				}
+				VALUE selfVal = selfIter.next(), otherVal = otherIter.next();
+				
+			}
+			if(otherIter.hasNext()){
+				return false;
+			}
+			return true;			
+		} catch (ClassCastException e){
+			return false;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		String result = "[";
+		for(VALUE v:this){
+			result += v.toString() + ",";
+		}
+		if(result.endsWith(",")){
+			result = result.substring(0, result.length()-1);
+		}
+		return result + "]";
+	}
+}
---
+++ src/com/fbehrens/dsal/elementare_datenstrukturen/DoublyLinkedListTest.java
@@ -0,0 +1,107 @@
+package com.fbehrens.dsal.elementare_datenstrukturen;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class DoublyLinkedListTest {
+
+	@Test
+	public void stringRep(){
+		DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+		assertEquals("[]", list.toString());
+		for(int i = 0; i < 5; i++){
+			list.append(i);
+		}
+		assertEquals("[0,1,2,3,4]", list.toString());
+	}
+	
+	@Test
+	public void equality() {
+		DoublyLinkedList<Integer> one = new DoublyLinkedList<>(), two = new DoublyLinkedList<>();
+		assertNotEquals(one, "A random object");
+		for(int i = 0; i < 5; i++){
+			one.append(i);
+			two.append(i);
+		}
+		assertEquals(one, two);
+		two.delete(2);
+		assertNotEquals(one, two);
+		one.delete(2);
+		assertEquals(one, two);
+	}
+	
+	@Test
+	public void insertion(){
+		// test whether appending to an empty list is the same as prepending in reverse order
+		DoublyLinkedList<Character> charsOne = new DoublyLinkedList<>();
+		for(char c = 'a'; c <= 'z'; c++){
+			charsOne.append(c);
+		}
+		DoublyLinkedList<Character> charsTwo = new DoublyLinkedList<>();
+		for(char c = 'z'; c >= 'a'; c--){
+			charsTwo.prepend(c);
+		}
+		assertEquals(charsOne, charsTwo);
+		
+		charsOne.append('1');
+		assertNotEquals(charsOne, charsTwo);
+		charsTwo.append('1');
+		assertEquals(charsOne, charsTwo);
+	}
+	
+	@Test
+	public void retrieval(){
+		// insert the numbers 0 to 1023 and then check whether they are stored in that oreder
+		DoublyLinkedList<Integer> ints = new DoublyLinkedList<>();
+		for(int i = 0; i < 1024; i++){
+			ints.append(i);
+		}
+		int i = 0;
+		for(int r : ints){
+			if(r != i){
+				fail("Numbers could not be retrieved in the same order as they were inserted!");
+			}
+			i++;
+		}
+		if(i != 1024){
+			fail("The number of retrieved elements was not correct!");
+		}
+	}
+	
+	@Test
+	public void deletion(){
+		DoublyLinkedList<String> strings = new DoublyLinkedList<>();
+		strings.delete("this should do nothing");
+		for(char c = 'a'; c <= 'z'; c++){
+			strings.append(c + "");
+		}
+		for(String c : new String[]{"f","l","o","r","i","a","n"}){
+			strings.delete(c);
+		}
+		DoublyLinkedList<String> stringsTwo = new DoublyLinkedList<>();
+		for(char c = 'a'; c <= 'z'; c++){
+			if(c != 'f' && c != 'l' && c != 'o' && c != 'r' && c != 'i' && c != 'a' && c != 'n'){
+				stringsTwo.append(c + "");
+			}
+		}
+		assertEquals(stringsTwo, strings);
+	}
+	
+	@Test
+	public void contains(){
+		DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+		for(int i = 0; i < 1024; i++){
+			list.prepend(i);
+		}
+		for(int i = 0; i < 1024; i++){
+			assertEquals(true, list.contains(i));
+		}
+		assertEquals(false, list.contains(0xE5E1));
+		assertEquals(false, list.contains(-1));
+	}
+	
+
+}
